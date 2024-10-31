a3850444ab6cf41dcbd9c16074b361f51eee081d
Def Def <defom_kills@mail.ru>
2024-10-31 21:43:47 +0300

    Создал новый класс для парсинга URL - VpnProfile.cs и кнопку в DashboardPage, которая будет в будущем подключаться к профилю впн

--- DefLink/DashboardPage.xaml.cs
+++ DefLink/DashboardPage.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using DefLink;
 
 namespace DefLink
 {
@@ -24,6 +13,29 @@ namespace DefLink
         public DashboardPage()
         {
             InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Ваш VLESS профиль
+                string vpnUri = "vless://7dff898a-147d-4a4b-83e5-17c9bf21174f@185.225.200.25:443?security=reality&type=tcp&headerType=&path=&host=&sni=www.ign.com&fp=chrome&pbk=J3ALGf6z4yIDgDyuvoURdPISvw3HBCd1xAObATB9y0U&sid=6318d4479bdc45c9#%F0%9F%9A%80%20Marz%20%28admin%29%20%5BVLESS%20-%20tcp%5D";
+
+                var profile = VpnProfile.Parse(vpnUri); // Метод для парсинга вашего VLESS профиля
+                ConnectToVpn(profile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка подключения: {ex.Message}");
+            }
+        }
+
+        private void ConnectToVpn(VpnProfile profile)
+        {
+            // Замените на вашу команду для подключения к VPN-клиенту
+            string command = $"your_vpn_client.exe --uuid {profile.UUID} --address {profile.Address} --port {profile.Port}";
+            Process.Start("cmd.exe", $"/C {command}");
         }
     }
 }
--- DefLink/MainWindow.xaml.cs
+++ DefLink/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
@@ -92,5 +93,6 @@ namespace DefLink
                 this.DragMove(); // Движение окна
             }
         }
+
     }
 }
